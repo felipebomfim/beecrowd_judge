003f7fb4195ad1a99be447da088a9d15bfa790cd
Felipe Bomfim <felipebomfim4161@gmail.com>
2025-08-10 18:40:26 -0300

    Completed 1020 in Java

---
+++ 1020/Main.java
@@ -0,0 +1,33 @@
+import java.io.IOException;
+import java.util.Scanner;
+ 
+/**
+ * IMPORTANT: 
+ *      O nome da classe deve ser "Main" para que a sua solução execute
+ *      Class name must be "Main" for your solution to execute
+ *      El nombre de la clase debe ser "Main" para que su solución ejecutar
+ */
+public class Main {
+ 
+    public static void main(String[] args) throws IOException {
+        
+        /**
+         * Escreva a sua solução aqui
+         * Code your solution here
+         * Escriba su solución aquí
+         */
+        Scanner sc = new Scanner(System.in);
+
+        int ageInDays = sc.nextInt();
+
+        int years = ageInDays/365; 
+        int months = (ageInDays - years*365)/30;
+        int days = (ageInDays - months*30 - years*365);
+
+        System.out.println(years + " ano(s)");
+        System.out.println(months + " mes(es)");
+        System.out.println(days + " dia(s)");
+ 
+    }
+ 
+}
