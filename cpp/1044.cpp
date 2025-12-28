d90804dd9668f5e4bb7052ef2eb8b913a56ef1e6
Felipe Bomfim <felipebomfim4161@gmail.com>
2025-12-28 13:17:28 -0300

    Completed 1044 in cpp

---
+++ cpp/1044.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+ 
+int main() {
+    int a, b;
+    cin >> a >> b;
+
+    if (b%a == 0 || a%b == 0)
+        cout << "Sao Multiplos\n";
+    else
+        cout << "Nao sao Multiplos\n";
+
+    return 0;
+}
